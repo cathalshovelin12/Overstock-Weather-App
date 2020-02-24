53237d0c3fc319adc27363d499dbacc98caa822d
Glenn <ggilmartin@overstock.com>
2020-02-24 09:34:13 +0000

    Adding mock data for design usage

--- lib/utils/openWeatherClient.dart
+++ lib/utils/openWeatherClient.dart
@@ -5,15 +5,27 @@ List<LocationData> getLocationData(){
   return[
     LocationData(
       location: Location.newYork,
-      temperature: 2008.00,
+      temperature: 208.00,
       isCelcius: false,
-      weatherIcon: "https://toppng.com/uploads/preview/image-setpng256x256-pxweather-icon-weather-flat-icon-115631356009rpwtrjbjj.png"
+      weatherIcon: "https://lh3.googleusercontent.com/proxy/ljfQpXZQGoSZeYW7mq6HqbZGrFzeJCrZ5A_m8TkTRxas-eZpR9YYAOvR8gadVD6NWTNKnCAsdatlfeRCyKzKpNhlMC4wzhlUn71M_6OHXdzOm0JRx3jxYIKgyvf-5BbtaBrHBvrEoGZ4"
+    ),
+    LocationData(
+      location: Location.newYork,
+      temperature: 18.00,
+      isCelcius: true,
+      weatherIcon: "https://www.pngfind.com/pngs/m/132-1323443_simple-weather-icons-cloudy-night-night-weather-icon.png"
     ),
     LocationData(
         location: Location.newYork,
         temperature: 23.00,
         isCelcius: true,
-        weatherIcon: "https://toppng.com/uploads/preview/image-setpng256x256-pxweather-icon-weather-flat-icon-115631356009rpwtrjbjj.png"
+        weatherIcon: "https://lh3.googleusercontent.com/proxy/ljfQpXZQGoSZeYW7mq6HqbZGrFzeJCrZ5A_m8TkTRxas-eZpR9YYAOvR8gadVD6NWTNKnCAsdatlfeRCyKzKpNhlMC4wzhlUn71M_6OHXdzOm0JRx3jxYIKgyvf-5BbtaBrHBvrEoGZ4"
+    ),
+    LocationData(
+        location: Location.newYork,
+        temperature: 68.00,
+        isCelcius: false,
+        weatherIcon: "https://www.pngfind.com/pngs/m/132-1323443_simple-weather-icons-cloudy-night-night-weather-icon.png"
     )
   ];
 }
